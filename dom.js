08101395e3d161de3c2c63ba8150f3bc0370787f
StefiLeon <stefaniapleon@gmail.com>
2021-06-14 14:30:56 -0300

    funcionalidad de botones

--- dom.js
+++ dom.js
@@ -1,24 +1,21 @@
-//Definir variables
-const iva = 1.21;
-
-//SESIÓN FOTOS PARTIDO
-const amistoso = 3000;
-const campeonato = 5000;
-
-//Crear boton
+/*Presupuesto fotos*/
+//Crear boton para presupuestar fotos
 const botonPresupuesto = document.createElement("button");
 botonPresupuesto.setAttribute("class", "btn btn-info btn-lg btn-block");
 botonPresupuesto.innerText = "Presupuestá";
+botonPresupuesto.style.marginBottom = "2em";
 botonPresupuesto.addEventListener("click", activarOpciones);
+
+//Nuevo boton
+const nuevoBoton = document.createElement("button");
+nuevoBoton.innerText = "Calcular presupuesto";
+nuevoBoton.style.paddingBottom = "5px";
+nuevoBoton.setAttribute("class", "btn btn-primary btn-block");
 
 //Activar opciones desde el DOM
 function activarOpciones() {
     botonPresupuesto.appendChild(tipoDePartido);
     botonPresupuesto.appendChild(cantidadDeJugadores);
-    const nuevoBoton = document.createElement("button");
-    nuevoBoton.innerText = "Calcular presupuesto";
-    nuevoBoton.style.paddingBottom = "5px";
-    nuevoBoton.setAttribute("class", "btn btn-primary btn-block");
     botonPresupuesto.appendChild(nuevoBoton);
     nuevoBoton.addEventListener("click", presupuestarFotos);
     botonPresupuesto.removeEventListener("click", activarOpciones);
@@ -48,36 +45,6 @@ opcionFutbol11.innerText = "Fútbol 11";
 cantidadDeJugadores.appendChild(opcionFutbol5);
 cantidadDeJugadores.appendChild(opcionFutbol7);
 cantidadDeJugadores.appendChild(opcionFutbol11);
-
-//Funcion para presupuestar fotos
-function presupuestarFotos() {
-    let presupuestoFotos = 0;
-    if (opcionAmistoso.selected) {
-        presupuestoFotos = 3000;
-    } else if (opcionCampeonato.selected) {
-        presupuestoFotos = 5000;
-    } else {
-        alert("Valor erróneo");
-    }
-    if (opcionFutbol11.selected) {
-        presupuestoFotos = presupuestoFotos * 1.8;
-    } else if (opcionFutbol7.selected) {
-        presupuestoFotos = presupuestoFotos * 1.5;
-    } else if (opcionFutbol5.selected) {
-        presupuestoFotos = presupuestoFotos / 1.0;
-    } else {
-        alert("Valor erróneo");
-    }
-    alert("El presupuesto con edición incluida es de $" + presupuestoFotos);
-}
-
-//IMPRESION DE FOTOS
-
-//COSTOS DE IMPRESION
-const fotoChica = 100;
-const fotoMediana = 115;
-const fotoGrande = 200;
-const poster = 1500;
 
 //Crear boton de impresion
 const botonVideo = document.createElement("button");
@@ -120,20 +87,3 @@ tipoDeImpresion.appendChild(opcionPoster);
 const cantidadDeFotos = document.createElement("input");
 cantidadDeFotos.setAttribute("class", "form-control");
 cantidadDeFotos.placeholder = "Ingrese la cantidad de fotos a imprimir";
-
-//Funcion para presupuestar impresiones
-function presupuestarImpresion() {
-    let presupuestoImpresion = 0;
-    if (opcionChica.selected) {
-        presupuestoImpresion = (fotoChica * iva);
-    } else if (opcionMediana.selected) {
-        presupuestoImpresion = (fotoMediana * iva);
-    } else if (opcionGrande.selected) {
-        presupuestoImpresion = (fotoGrande * iva)
-    } else if (opcionPoster.selected) {
-        presupuestoImpresion = (poster * iva);
-    } else {
-        alert("Valor erróneo");
-    }
-    alert("El presupuesto para " + cantidadDeFotos.value + " fotos es de $" + (presupuestoImpresion*parseInt(cantidadDeFotos.value)));
-}
---
+++ main.js
@@ -0,0 +1,106 @@
+//Definir variables
+const iva = 1.21;
+
+//SESIÓN FOTOS PARTIDO
+const amistoso = 3000;
+const campeonato = 5000;
+let presupuestoFotos = 0;
+let contadorFotos = 0;
+
+/*Presupuesto impresiones*/
+//COSTOS DE IMPRESION
+const fotoChica = 100;
+const fotoMediana = 115;
+const fotoGrande = 200;
+const poster = 1500;
+let presupuestoImpresion = 0;
+let contadorImpresion = 0;
+
+//Funcion para presupuestar fotos
+function presupuestarFotos() {
+    //let presupuestoFotos = 0;
+    if (opcionAmistoso.selected) {
+        presupuestoFotos = 3000;
+    } else if (opcionCampeonato.selected) {
+        presupuestoFotos = 5000;
+    } else {
+        alert("Valor erróneo");
+    }
+    if (opcionFutbol11.selected) {
+        presupuestoFotos = presupuestoFotos * 1.8;
+    } else if (opcionFutbol7.selected) {
+        presupuestoFotos = presupuestoFotos * 1.5;
+    } else if (opcionFutbol5.selected) {
+        presupuestoFotos = presupuestoFotos / 1.0;
+    } else {
+        alert("Valor erróneo");
+    }
+    const presupuestoTotalFotos = document.createElement('p');
+    const botonDeCompraFotos = document.createElement("button");
+    presupuestoTotalFotos.id = "presupuestoTotalFoto";
+    presupuestoTotalFotos.innerHTML = "El presupuesto total es de $" + presupuestoFotos;
+    presupuestoTotalFotos.style.paddingTop = "15px";
+    presupuestoTotalFotos.style.color = "white";
+    presupuestoTotalFotos.style.fontSize = "large";
+    botonDeCompraFotos.id = "botonDeCompraFotos";
+    botonDeCompraFotos.innerHTML = "Comprar";
+    botonDeCompraFotos.setAttribute("class", "btn btn-light btn-block");
+    botonDeCompraFotos.addEventListener("click", guardarEnCarritoFotos);
+    if(contadorFotos>0) {
+        document.getElementById("presupuestoTotalFoto").innerHTML = "El precio total es de $" + presupuestoFotos;
+        document.getElementById("botonDeCompraFotos").innerHTML = "Comprar";
+        botonDeCompraFotos.addEventListener("click", guardarEnCarritoFotos);
+        return;
+    }
+    contadorFotos++;
+    botonPresupuesto.appendChild(presupuestoTotalFotos);
+    botonPresupuesto.appendChild(botonDeCompraFotos);
+    return presupuestoFotos;
+}
+
+//Funcion para presupuestar impresiones
+function presupuestarImpresion() {
+    if (opcionChica.selected) {
+        presupuestoImpresion = (fotoChica * iva);
+    } else if (opcionMediana.selected) {
+        presupuestoImpresion = (fotoMediana * iva);
+    } else if (opcionGrande.selected) {
+        presupuestoImpresion = (fotoGrande * iva)
+    } else if (opcionPoster.selected) {
+        presupuestoImpresion = (poster * iva);
+    } else {
+        alert("Valor erróneo");
+    }
+    const presupuestoTotalImpresion = document.createElement('p');
+    const botonDeCompraImpresion = document.createElement("button");
+    presupuestoTotalImpresion.id = "presupuestoTotalImpresion";
+    presupuestoTotalImpresion.innerHTML = "El presupuesto total es de $" + presupuestoImpresion;
+    presupuestoTotalImpresion.style.paddingTop = "15px";
+    presupuestoTotalImpresion.style.color = "white";
+    presupuestoTotalImpresion.style.fontSize = "large";
+    botonDeCompraImpresion.id = "botonDeCompraImpresion";
+    botonDeCompraImpresion.innerHTML = "Comprar";
+    botonDeCompraImpresion.setAttribute("class", "btn btn-light btn-block");
+    botonDeCompraImpresion.addEventListener("click", guardarEnCarritoImpresion);
+    if(contadorImpresion>0) {
+        document.getElementById("presupuestoTotalImpresion").innerHTML = "El precio total es de $" + presupuestoImpresion;
+        document.getElementById("botonDeCompraImpresion").innerHTML = "Comprar";
+        botonDeCompraImpresion.addEventListener("click", guardarEnCarritoImpresion);
+        return;
+    }
+    contadorImpresion++;
+    botonVideo.appendChild(presupuestoTotalImpresion);
+    botonVideo.appendChild(botonDeCompraImpresion);
+    return presupuestoImpresion;
+}
+
+//Funcion para guardar en carrito
+function guardarEnCarritoFotos() {
+    console.log(presupuestoFotos);
+    localStorage.setItem('compraFotos', parseInt(presupuestoFotos));
+}
+
+function guardarEnCarritoImpresion() {
+    console.log(presupuestoImpresion);
+    localStorage.setItem('compraImpresion', parseInt(presupuestoImpresion));
+}
